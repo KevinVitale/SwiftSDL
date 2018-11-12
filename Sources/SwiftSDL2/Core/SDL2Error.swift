399dee40981ae537e0966b1d60b42bde4bcaf09d
Kevin Vitale <kevinvitale@gmail.com>
2018-11-12 10:46:58 -0500

    [Core] renamed 'Error' to 'SDL2Error'

--- Sources/SwiftSDL2/Core/SDL2Error.swift
+++ Sources/SwiftSDL2/Core/SDL2Error.swift
@@ -1,8 +1,12 @@
 import Clibsdl2
 
-enum Error: Swift.Error, CustomStringConvertible
+enum SDL2Error: Swift.Error, CustomStringConvertible
 {
     case error
+    
+    static func clear() {
+        SDL_ClearError()
+    }
     
     var description: String {
         return String(cString: SDL_GetError())
