1c3048f96fd42fcbaf68baad6cbc1155d076f2f6
Kevin Vitale <kevinvitale@gmail.com>
2018-11-12 10:48:10 -0500

    [Color] formatting change

--- Sources/SwiftSDL2/Video/Color.swift
+++ Sources/SwiftSDL2/Video/Color.swift
@@ -2,7 +2,8 @@ import Clibsdl2
 
 extension SDL_Color
 {
-    static func random(alpha a: UInt8 = 0xFF) -> SDL_Color {
+    static func random(alpha a: UInt8 = 0xFF) -> SDL_Color
+    {
         let r = UInt8(arc4random_uniform(256))
         let g = UInt8(arc4random_uniform(256))
         let b = UInt8(arc4random_uniform(256))
