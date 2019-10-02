dc9fe288759a409e89cb3b65a4821d0481a9a8a6
Kevin Vitale <kevinvitale@gmail.com>
2019-10-02 13:06:45 -0400

    [Surface] add computed properties

--- Sources/SwiftSDL2/Color.swift
+++ Sources/SwiftSDL2/Color.swift
@@ -9,4 +9,12 @@ public extension SDL_Color
         let b = UInt8(arc4random_uniform(256))
         return SDL_Color(r: r, g: g, b: b, a: a)
     }
+    
+    func mapRGB(format: UnsafeMutablePointer<SDL_PixelFormat>!) -> UInt32 {
+        return SDL_MapRGB(format, self.r, self.g, self.b)
+    }
+    
+    func mapRGBA(format: UnsafeMutablePointer<SDL_PixelFormat>!) -> UInt32 {
+        return SDL_MapRGBA(format, self.r, self.g, self.b, self.a)
+    }
 }
--- Sources/SwiftSDL2/Surface.swift
+++ Sources/SwiftSDL2/Surface.swift
@@ -10,18 +10,72 @@ public struct SDLSurface: SDLType {
 public typealias Surface = SDLPointer<SDLSurface>
 
 public extension SDLPointer where T == SDLSurface {
+    init?(width: Int32, height: Int32, depth: Int32, redMask: UInt32 = .min, greenMask: UInt32 = .min, blueMask: UInt32 = .min, alphaMask: UInt32 = .min) {
+        return nil
+    }
+    
+    init?(width: Int32, height: Int32, format: UInt32) {
+        return nil
+    }
+    
+    init?(pixels: UnsafeMutableRawPointer!, width: Int32, height: Int32, depth: Int32, pitch: Int32, redMask: UInt32, greenMask: UInt32, blueMask: UInt32, alphaMask: UInt32) {
+        return nil
+    }
+    
+    init?(pixels: UnsafeMutableRawPointer!, width: Int32, height: Int32, pitch: Int32, format: UInt32) {
+        return nil
+    }
+
     static func surface(forWindow window: Window) -> Result<Surface, Error> {
         guard let surface = SDL_GetWindowSurface(window._pointer) else {
             return .failure(SDLError.error(Thread.callStackSymbols))
         }
         return .success(Self(pointer: surface))
     }
+
+    var clippingRect: SDL_Rect {
+        return _pointer.pointee.clip_rect
+    }
     
+    var format: UnsafeMutablePointer<SDL_PixelFormat>! {
+        return _pointer.pointee.format
+    }
+    
+    var height: Int32 {
+        return _pointer.pointee.h
+    }
+    
+    var width: Int32 {
+        return _pointer.pointee.w
+    }
+
+    var pitch: Int32 {
+        return _pointer.pointee.pitch
+    }
+    
+    var userData: UnsafeMutableRawPointer! {
+        get { _pointer.pointee.userdata }
+        set { _pointer.pointee.userdata = newValue }
+    }
+
     func lock() -> Bool {
         SDL_LockSurface(_pointer) == -1 ? false : true
     }
     
     func unlock() {
         SDL_UnlockSurface(_pointer)
+    }
+    
+    func fill(rect fillRect: SDL_Rect? = nil, color: SDL_Color) throws {
+        let rect: UnsafePointer<SDL_Rect>! = withUnsafePointer(to: fillRect) {
+            guard $0.pointee != nil else {
+                return nil
+            }
+            return $0.withMemoryRebound(to: SDL_Rect.self, capacity: 1) { return $0 }
+        }
+
+        guard SDL_FillRect(_pointer, rect, color.mapRGB(format: self.format)) == 0 else {
+            throw SDLError.error(Thread.callStackSymbols)
+        }
     }
 }
