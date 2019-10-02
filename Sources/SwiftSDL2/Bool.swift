df9b4c672d9b67555ee19d0b8ae8a0dc648c3c17
Kevin Vitale <kevinvitale@gmail.com>
2019-10-02 16:30:00 -0400

    [SDL] adds 'Hint' enum

--- Sources/SwiftSDL2/Bool.swift
+++ Sources/SwiftSDL2/Bool.swift
@@ -1,7 +1,16 @@
 import CSDL2
 
-extension Bool {
+public extension Bool {
     var SDLBool: SDL_bool {
         return self == true ? SDL_TRUE : SDL_FALSE
+    }
+}
+
+public extension SDL_bool {
+    var bool: Bool {
+        switch self {
+        case SDL_TRUE: return true
+        default: return false
+        }
     }
 }
