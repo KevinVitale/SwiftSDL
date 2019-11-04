fb0e8f0e0d927566210549d7da0c233221430075
Kevin Vitale <kevinvitale@gmail.com>
2019-11-04 15:25:58 -0500

    [Surface] subclasses 'SDLPointer'

--- Sources/SwiftSDL2/Surface.swift
+++ Sources/SwiftSDL2/Surface.swift
@@ -1,14 +1,12 @@
 import CSDL2
 
-public typealias Surface = SDLPointer<SDLSurface>
-
-public struct SDLSurface: SDLType {
+public class Surface: SDLPointer<Surface>, SDLType {
     public static func destroy(pointer: UnsafeMutablePointer<SDL_Surface>) {
         SDL_FreeSurface(pointer)
     }
 }
 
-public extension SDLPointer where T == SDLSurface {
+public extension Surface {
     func copyPixelFormat() -> PixelFormat {
         self.pass { SDL_AllocFormat($0.pointee.format.pointee.format) }
             .map(PixelFormat.init)!
