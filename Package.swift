90cb56ab36dff7ac2c5607698d70863686bf7408
Kevin Vitale <kevinvitale@gmail.com>
2019-12-19 11:52:32 -0500

    [MIXER] adds 'Mixer' dependency

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     targets: [
       .target( name: "DemoSDL2", dependencies: ["SwiftSDL2"]), 
       .target( name: "CreateWindowDemo", dependencies: ["SwiftSDL2"]), 
-      .target( name: "SwiftSDL2", dependencies: ["CSDL2", "CSDL2_Image"]),
+      .target( name: "SwiftSDL2", dependencies: ["CSDL2", "CSDL2_Image", "CSDL2_Mixer"]),
       .systemLibrary( name: "CSDL2", pkgConfig: "sdl2"
         , providers: [
             .brew(["sdl2"]),
@@ -21,6 +21,11 @@ let package = Package(
         , providers: [
             .brew(["sdl2_image"]),
             .apt(["libsdl2_image-dev"]) ]
+      ),
+      .systemLibrary( name: "CSDL2_Mixer", pkgConfig: "sdl2_mixer"
+        , providers: [
+            .brew(["sdl2_mixer"]),
+            .apt(["libsdl2_mixer-dev"]) ]
       ),
       //.testTarget( name: "SDLTests", dependencies: ["SDL"]),
     ],
---
+++ Sources/CSDL2_Mixer/shim.h
@@ -0,0 +1,5 @@
+#ifdef __APPLE__
+#include "/usr/local/include/SDL2/SDL_mixer.h"
+#else
+#include <SDL2/SDL_mixer.h>
+#endif
