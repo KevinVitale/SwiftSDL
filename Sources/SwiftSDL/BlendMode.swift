01cb2027a4a00a77dbd5c64a1bffa6230923988f
Kevin Vitale <kevinvitale@gmail.com>
2024-12-03 07:05:37 -0500

    [Blend] adds 'SDL_{BlendOperation|Factor}'

--- Sources/SwiftSDL/BlendMode.swift
+++ Sources/SwiftSDL/BlendMode.swift
@@ -1,4 +1,4 @@
-public enum SDL_BlendMode: RawRepresentable, CustomDebugStringConvertible {
+public enum SDL_BlendMode: RawRepresentable, CustomDebugStringConvertible, Sendable {
   case none
   case blend
   case blendPremul
@@ -64,4 +64,64 @@ public enum SDL_BlendMode: RawRepresentable, CustomDebugStringConvertible {
       case .custom:       return "custom"
     }
   }
+}
+
+extension SDL_BlendOperation: @retroactive CustomDebugStringConvertible {
+  /** dst + src: supported by all renderers */
+  public static let add = SDL_BLENDOPERATION_ADD
+  
+  /** src - dst : supported by D3D, OpenGL, OpenGLES, and Vulkan */
+  public static let substract = SDL_BLENDOPERATION_SUBTRACT
+  
+  /** dst - src : supported by D3D, OpenGL, OpenGLES, and Vulkan */
+  public static let reverseSubtract = SDL_BLENDOPERATION_REV_SUBTRACT
+  
+  /** min(dst, src) : supported by D3D, OpenGL, OpenGLES, and Vulkan */
+  public static let minium = SDL_BLENDOPERATION_MINIMUM
+  
+  /** max(dst, src) : supported by D3D, OpenGL, OpenGLES, and Vulkan */
+  public static let maximum = SDL_BLENDOPERATION_MAXIMUM
+  
+  public var debugDescription: String {
+    switch self {
+      case SDL_BLENDOPERATION_ADD: return "add"
+      case SDL_BLENDOPERATION_SUBTRACT: return "subtract"
+      case SDL_BLENDOPERATION_REV_SUBTRACT: return "reverse subtract"
+      case SDL_BLENDOPERATION_MINIMUM: return "minimum"
+      case SDL_BLENDOPERATION_MAXIMUM: return "maximum"
+      default: return "Unknown SDL_BlendOperation: \(self)"
+    }
+  }
+}
+
+extension SDL_BlendFactor {
+  /** 0, 0, 0, 0 */
+  public static let zeor = SDL_BLENDFACTOR_ZERO
+  
+  /** 1, 1, 1, 1 */
+  public static let one = SDL_BLENDFACTOR_ONE
+  
+  /** srcR, srcG, srcB, srcA */
+  public static let sourceColor = SDL_BLENDFACTOR_SRC_COLOR
+  
+  /** 1-srcR, 1-srcG, 1-srcB, 1-srcA */
+  public static let oneMinusSourceColor = SDL_BLENDFACTOR_ONE_MINUS_SRC_COLOR
+  
+  /** srcA, srcA, srcA, srcA */
+  public static let alpha = SDL_BLENDFACTOR_SRC_ALPHA
+  
+  /** 1-srcA, 1-srcA, 1-srcA, 1-srcA */
+  public static let oneMinusSourceAlpha = SDL_BLENDFACTOR_ONE_MINUS_SRC_ALPHA
+  
+  /** dstR, dstG, dstB, dstA */
+  public static let destinationColor = SDL_BLENDFACTOR_DST_COLOR
+  
+  /** 1-dstR, 1-dstG, 1-dstB, 1-dstA */
+  public static let oneMinusDestinationColor = SDL_BLENDFACTOR_ONE_MINUS_DST_COLOR
+  
+  /** dstA, dstA, dstA, dstA */
+  public static let destinationAlpha = SDL_BLENDFACTOR_DST_ALPHA
+  
+  /** 1-dstA, 1-dstA, 1-dstA, 1-dstA */
+  public static let oneMinusDestinationAlpha = SDL_BLENDFACTOR_ONE_MINUS_DST_ALPHA
 }
